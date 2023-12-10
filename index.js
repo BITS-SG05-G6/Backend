f4e468a1fb1f27216eb272abe74f2dc58392f03f
JuneDay <tthong.vox@gmail.com>
2023-12-10 11:00:01 +0700

    Create User Schema, Middle Ware handling for Login and Sign Up Feature

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-require('dotenv').config();
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const authRoutes = require("./routes/auth");
+const cors = require("cors");
+require("dotenv").config();
 
 // MIDDLEWARE
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 //CONNECT DATABASE
-mongoose.connect(process.env.DATABASE)
-.then(() => console.log('DB connected'))
-.catch((err) => console.log(err));
+mongoose
+  .connect(process.env.DATABASE)
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.log(err));
 
 // IMPORT ROUTES
+app.use("/auth", authRoutes);
 
 // ROUTES MIDDLEWARE
-
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
   console.log(`App listening on port http://localhost:${port}/api`);
 });
-
 
 module.exports = app;
