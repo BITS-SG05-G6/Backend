53d3d2cd208a6c9c3df50f97af8f668cbfba0652
HaAnh18 <102302068+HaAnh18@users.noreply.github.com>
2023-12-25 23:26:12 +0700

    done logout function

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -9,6 +9,8 @@ router.post("/signup", user.signup);
 // Login route
 router.post("/signin", user.signin);
 
+router.post("/signout", user.signout);
+
 router.get("/profile", isAuthenticated, user.getProfile);
 
 module.exports = router;
