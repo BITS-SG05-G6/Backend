f5a38388f0e21c5e1b6b82babf1407da53a9c34a
vntuananh <vntuananhbui@gmail.com>
2023-12-28 17:11:24 +0700

    update route name

--- routes/statisticRoute.js
+++ routes/statisticRoute.js
@@ -4,46 +4,46 @@ const statistic = require("../controllers/statisticControllers");
 const {isAuthenticated} = require("../middlewares/auth");
 
 //Chart1: Trend Statistic
-router.get('/chart1/week/:userId', statistic.getTransactionStatistics7days);
+router.get('/trend/week/:userId', statistic.getTransactionStatistics7days);
 
-router.get('/chart1/thismonth/:userId', statistic.getTransactionStatisticsThisMonth);
+router.get('/trend/thismonth/:userId', statistic.getTransactionStatisticsThisMonth);
 
-router.get('/chart1/lastmonth/:userId', statistic.getTransactionStatisticsLastMonth);
+router.get('/trend/lastmonth/:userId', statistic.getTransactionStatisticsLastMonth);
 
 //Chart 2: Distribution Data
-router.get('/chart2/total/:userId', statistic.expensesFrequencyDistribution);
+router.get('/distribution/total/:userId', statistic.expensesFrequencyDistribution);
 
-router.get('/chart2/week/:userId', statistic.expensesFrequencyDistributionLastWeek);
+router.get('/distribution/week/:userId', statistic.expensesFrequencyDistributionLastWeek);
 
-router.get('/chart2/month/:userId', statistic.expensesFrequencyDistributionLastMonth);
+router.get('/distribution/month/:userId', statistic.expensesFrequencyDistributionLastMonth);
 
 //Chart 3: Category Income
-router.get('/chart3/total/:userId', statistic.getTotalIncomeByCategory);
+router.get('/catin/total/:userId', statistic.getTotalIncomeByCategory);
 
-router.get('/chart3/lastweek/:userId', statistic.getTotalIncomeByCategoryLastWeek);
+router.get('/catin/lastweek/:userId', statistic.getTotalIncomeByCategoryLastWeek);
 
-router.get('/chart3/lastmonth/:userId', statistic.getTotalIncomeByCategoryLastMonth);
+router.get('/catin/lastmonth/:userId', statistic.getTotalIncomeByCategoryLastMonth);
 
 
 //Chart 4: Category Expense
-router.get('/chart4/total/:userId', statistic.getTotalExpenseByCategory);
+router.get('/catex/total/:userId', statistic.getTotalExpenseByCategory);
 
-router.get('/chart4/lastweek/:userId', statistic.getTotalExpenseByCategoryLastWeek);
+router.get('/catex/lastweek/:userId', statistic.getTotalExpenseByCategoryLastWeek);
 
-router.get('/chart4/lastmonth/:userId', statistic.getTotalExpenseByCategoryLastMonth);
+router.get('/catex/lastmonth/:userId', statistic.getTotalExpenseByCategoryLastMonth);
 
 //Chart 5: Income/Expense Ratio
-router.get('/chart5/month/:userId', statistic.getCompareExpanseIncomeByMonth);
+router.get('/exin/month/:userId', statistic.getCompareExpanseIncomeByMonth);
 
-router.get('/chart5/week/:userId', statistic.getCompareExpanseIncomeByWeek);
+router.get('/exin/week/:userId', statistic.getCompareExpanseIncomeByWeek);
 
-router.get('/chart5/total/:userId', statistic.getCompareExpanseIncomeTotal);
+router.get('/exin/total/:userId', statistic.getCompareExpanseIncomeTotal);
 
 //Chart 6: Wallet
-router.get('/chart6/week/:userId', statistic.getTotalExpenseByWalletLastWeek);
+router.get('/walletex/week/:userId', statistic.getTotalExpenseByWalletLastWeek);
 
-router.get('/chart6/month/:userId', statistic.getTotalExpenseByWalletLastMonth);
+router.get('/walletex/month/:userId', statistic.getTotalExpenseByWalletLastMonth);
 
-router.get('/chart6/total/:userId', statistic.getTotalExpenseByWallet);
+router.get('/walletex/total/:userId', statistic.getTotalExpenseByWallet);
 
 module.exports = router;
