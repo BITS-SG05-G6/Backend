eea09516a17427e8d0ad823b6b26aab8281e89c9
Vo Thanh Thong <thong@Air-cua-Vo.STUSGS.RMIT.EDU.VN>
2024-01-09 12:43:44 +0700

    Hot Fix User Profile

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const user = require("../controllers/userControllers")
-const {isAuthenticated} = require("../middlewares/auth");
+const user = require("../controllers/userControllers");
+const { isAuthenticated } = require("../middlewares/auth");
 
 // Signup route
 router.post("/signup", user.signup);
@@ -12,5 +12,8 @@ router.post("/signin", user.signin);
 router.post("/signout", user.signout);
 
 router.get("/profile", isAuthenticated, user.getProfile);
+
+// Update Profile
+router.put("/updateprofile", isAuthenticated, user.updateProfile);
 
 module.exports = router;
