e8d09cbab6d54c15d9c6094a17ccddcf4714bc6a
xian-94 <s3957034@rmit.edu.vn>
2024-01-10 11:12:45 +0700

    add get a wallet function

--- routes/walletRoute.js
+++ routes/walletRoute.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const wallet = require("../controllers/walletController");
-const {isAuthenticated} = require("../middlewares/auth");
+const { isAuthenticated } = require("../middlewares/auth");
 
 //Add Wallet
 router.post("/", isAuthenticated, wallet.createWallet);
 
-router.get("/view", isAuthenticated, wallet.getWallet);
+router.get("/view", isAuthenticated, wallet.getWallets);
+
+router.get("/view/:id", isAuthenticated, wallet.getWallet);
 
 router.delete("/delete/:id", isAuthenticated, wallet.deleteWallet);
 
